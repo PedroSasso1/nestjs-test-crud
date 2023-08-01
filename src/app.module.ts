dd517b12c0a2bd20c67838643db46182c70a966b
pedrosasso1 <pedrosassom@outlook.com>
2023-07-31 22:40:31 -0300

    chore: start despesas crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { TweetsModule } from './tweets/tweets.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { DespesasModule } from './despesas/despesas.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TweetsModule,
     MongooseModule.forRoot(process.env.MONGO_DSN),
+    DespesasModule,
   ],
   controllers: [AppController],
   providers: [AppService],
